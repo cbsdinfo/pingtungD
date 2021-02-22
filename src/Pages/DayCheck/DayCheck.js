bc4b3db7929e53219c8400b3dd2e123fbd262c57
Arhua <a0987837233@gmail.com>
2021-02-22 13:31:29 +0800

    簽名一半

--- src/Pages/DayCheck/DayCheck.js
+++ src/Pages/DayCheck/DayCheck.js
@@ -1,10 +1,10 @@
 import React, { useContext, useState, useEffect, useCallback } from 'react';
 import { Context } from '../../Store/Store'
 import { SubContainer, globalContextService, Text, FormContainer, FormRow, TextInput, modalsService } from '../../Components';
-import { LaptopL } from './RWD/LaptopL';
-import { Laptop } from './RWD/Laptop';
+// import { LaptopL } from './RWD/LaptopL';
+// import { Laptop } from './RWD/Laptop';
 import { MobileM } from './RWD/MobileM';
-import { Tablet } from './RWD/Tablet';
+// import { Tablet } from './RWD/Tablet';
 import { useWindowSize } from '../../SelfHooks/useWindowSize';
 import { clearLocalStorage, clearSession, getParseItemLocalStorage, valid } from '../../Handlers';
 import { useHistory } from 'react-router-dom';
@@ -194,7 +194,7 @@ export const DayCheck = (props) => {
 
     return (
         <>
-            {
+            {/* {
                 768 <= Width &&
                 <LaptopL
                     NowTab={NowTab} // 目前公告頁面
@@ -205,7 +205,7 @@ export const DayCheck = (props) => {
                     setCheckDetail={setCheckDetail} // 設定詳細資料
                     GetNewsTypeExecute={GetNewsTypeExecute} // 選單更新值調用，取得特定類別所有最新消息
                 />
-            }
+            } */}
             {/* {
                 (1024 <= Width && Width < 1440) &&
                 <Laptop
@@ -221,7 +221,7 @@ export const DayCheck = (props) => {
                 />
             } */}
             {
-                Width < 768 &&
+                // Width < 768 &&
                 <MobileM
                     NowTab={NowTab} // 目前公告頁面
                     setNowTab={setNowTab} // 設定目前公告頁面
--- src/Pages/DayCheck/RWD/LaptopL.js
+++ src/Pages/DayCheck/RWD/LaptopL.js
@@ -32,104 +32,10 @@ const LaptopLBase = (props) => {
                 theme={laptopL.mainPageContainer}
                 outSideTopComponent={
                     <>
-                        {/* 標題列 */}
-                        <BasicContainer
-                            theme={laptopL.whiteContainer}
-                        >
-                            <BasicContainer
-                                theme={laptopL.tabsContainer}
-                            >
-                                {
-                                    // <BasicContainer>
-
-                                    tabMap().map((item, index) => {
-                                        return (
-                                            <React.Fragment key={index}>
-                                                <Text
-                                                    onClick={() => {
-                                                        if (props.NowTab !== item) {
-                                                            props.setNowTab(item);
-                                                            props.GetNewsTypeExecute(true, props.NewsType[index]?.value, globalContextService.get("SystemNewsComponentPage", "DateTimeRange"));
-                                                        }
-                                                    }}
-                                                    isActive={props.NowTab === item}
-                                                    theme={laptopL.titleBarCallCarTab}
-                                                >
-                                                    {item}
-                                                </Text>
-                                            </React.Fragment>
-                                        )
-                                    })
-
-                                    // </BasicContainer>
-                                }
-                            </BasicContainer>
-                        </BasicContainer>
                     </>
                 }
             >
-                {/* 切換使用的組件 */}
-                {/* {tabMap("tabUseComponent")?.[props.nowTab]} */}
-                {/* {console.log(props.NewsType)} */}
-                {/* {console.log(props.AllNews)} */}
-                {!isUndefined(props?.CheckDetail?.title)
-                    ?
-                    <>
-                        <Container>
-                            {/* 詳細資料外側容器 */}
-                            <BasicContainer
-                                height={Height}
-                                theme={laptopL.detailOutContainer}
-                            >
-                                {/* 詳細資料容器 */}
-                                <BasicContainer
-                                    theme={laptopL.detailContainer}
-                                >
-                                    {/* 詳細資料 標題 */}
-                                    <Text
-                                        theme={laptopL.detailHeader}
-                                    >
-                                        {props.CheckDetail.title}
-                                    </Text>
-
-                                    {/* 詳細資料 內文 */}
-                                    <TextEditor
-                                        viewType
-                                        value={props.CheckDetail.contents?.replaceAll('<img', `<img style="max-width:100%" `)}
-
-                                        // onChange={(e, value, onInitial) => {
-                                        //     console.log(value)
-                                        //     globalContextService.set("NewsAddPage", "NewsEditor", value)
-                                        // }}
-                                        // placeholder={'請輸入最新消息內容...'}
-                                        theme={laptopL.newsEditor}
-                                    />
-                                </BasicContainer>
-
-                                {/* 回列表按鈕 */}
-                                <NativeLineButton
-                                    baseDefaultTheme={"DefaultTheme"}
-                                    disable={false}
-                                    type="button" // 防止提交
-                                    theme={laptopL.returnButton}
-                                    onClick={() => {
-                                        props.setCheckDetail({})
-                                    }}
-                                >
-                                    回列表
-                                </NativeLineButton>
-                            </BasicContainer>
-                        </Container>
-                    </>
-                    :
-                    <SystemNewsComponent
-                        AllNews={props.AllNews} // 類別下所有最新消息
-                        NowTab={props.NewsType.filter((it) => (it.label === props.NowTab))?.[0]}
-                        GetNewsTypeExecute={props.GetNewsTypeExecute}
-                        CheckDetail={props.CheckDetail} // 詳細資料
-                        setCheckDetail={props.setCheckDetail} // 設定詳細資料
-                    />
-                }
+                
             </MainPageContainer>
         </>
     )
